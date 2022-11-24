ea17ff15ae0ccf1b0362541d36b8593b14f48cd5
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-24 19:30:50 +0000

    Fix capitalize test

--- algo.js
+++ algo.js
@@ -1,13 +1,18 @@
-function Algo() {}
+function Algo() { }
 
-Algo.prototype.reverse = function(str) {
+Algo.prototype.reverse = function (str) {
   return str.split('').reverse().join('')
 };
 
-Algo.prototype.isPalindrome = function(str) {
+Algo.prototype.isPalindrome = function (str) {
   return this.reverse(str) === str
 };
 
-Algo.prototype.capitalize = function(str) {};
+Algo.prototype.capitalize = function (str) {
+  return str
+    .split(' ')
+    .map(it => it[0].toUpperCase() + it.substring(1))
+    .join(' ')
+};
 
 module.exports = Algo;
